b6caa65b735149a4851c0af4a09d8cedcdf10b8d
Beatle <123680498+DavidCoderror@users.noreply.github.com>
2025-03-20 12:38:41 -0400

    Create click-Me.py

---
+++ click-Me.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+fenetre=tk.Tk()
+fenetre.title("Testing")
+
+def cliqueCalculer():
+    lblReponse1['text'] = "Thank You"
+
+lblReponse1 = tk.Label(fenetre)
+lblReponse1["text"] = "Hello There."
+lblReponse1.pack()
+
+btnCalculer1 = tk.Button(fenetre)
+btnCalculer1["text"] = " Click Me"
+btnCalculer1["font"] = "Arial 11"
+btnCalculer1["command"] = cliqueCalculer
+btnCalculer1.pack()
+
+fenetre.mainloop()
